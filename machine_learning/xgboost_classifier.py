8a06dae17daf818c253ec46d6e036f74dd54781c
Apurva Chavan <84921911+Apurva1205@users.noreply.github.com>
2023-03-25 14:43:18 +0530

    Update xgboost_classifier.py

--- machine_learning/xgboost_classifier.py
+++ machine_learning/xgboost_classifier.py
@@ -3,65 +3,60 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
 from xgboost import XGBClassifier
 
 
 def data_handling(data: dict) -> tuple:
     # Split dataset into features and target
     # data is features
-    """
-    >>> data_handling(({'data':'[5.1, 3.5, 1.4, 0.2]','target':([0])}))
-    ('[5.1, 3.5, 1.4, 0.2]', [0])
-    >>> data_handling(
-    ...     {'data': '[4.9, 3.0, 1.4, 0.2], [4.7, 3.2, 1.3, 0.2]', 'target': ([0, 0])}
-    ... )
-    ('[4.9, 3.0, 1.4, 0.2], [4.7, 3.2, 1.3, 0.2]', [0, 0])
-    """
     return (data["data"], data["target"])
 
 
 def xgboost(features: np.ndarray, target: np.ndarray) -> XGBClassifier:
-    """
-    # THIS TEST IS BROKEN!! >>> xgboost(np.array([[5.1, 3.6, 1.4, 0.2]]), np.array([0]))
-    XGBClassifier(base_score=0.5, booster='gbtree', callbacks=None,
-                  colsample_bylevel=1, colsample_bynode=1, colsample_bytree=1,
-                  early_stopping_rounds=None, enable_categorical=False,
-                  eval_metric=None, gamma=0, gpu_id=-1, grow_policy='depthwise',
-                  importance_type=None, interaction_constraints='',
-                  learning_rate=0.300000012, max_bin=256, max_cat_to_onehot=4,
-                  max_delta_step=0, max_depth=6, max_leaves=0, min_child_weight=1,
-                  missing=nan, monotone_constraints='()', n_estimators=100,
-                  n_jobs=0, num_parallel_tree=1, predictor='auto', random_state=0,
-                  reg_alpha=0, reg_lambda=1, ...)
-    """
-    classifier = XGBClassifier()
-    classifier.fit(features, target)
-    return classifier
+    # Scale the features using StandardScaler
+    scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)
+
+    # Define the XGBoost classifier and the grid of hyperparameters to search
+    xgb = XGBClassifier()
+    param_grid = {
+        "learning_rate": [0.05, 0.1, 0.2],
+        "n_estimators": [50, 100, 200],
+        "max_depth": [3, 4, 5],
+        "gamma": [0, 0.1, 0.2],
+    }
+
+    # Perform a grid search with cross-validation to find the best hyperparameters
+    grid_search = GridSearchCV(
+        xgb, param_grid, scoring="accuracy", cv=5, n_jobs=-1, verbose=1
+    )
+    grid_search.fit(features_scaled, target)
+
+    # Print the best hyperparameters and the corresponding accuracy score
+    print("Best hyperparameters:", grid_search.best_params_)
+    print("Best accuracy score:", grid_search.best_score_)
+
+    # Return the XGBoost classifier with the best hyperparameters
+    return grid_search.best_estimator_
 
 
 def main() -> None:
-    """
-    >>> main()
-
-    Url for the algorithm:
-    https://xgboost.readthedocs.io/en/stable/
-    Iris type dataset is used to demonstrate algorithm.
-    """
-
-    # Load Iris dataset
+    # Load the Iris dataset
     iris = load_iris()
     features, targets = data_handling(iris)
+
+    # Split the dataset into training and test sets
     x_train, x_test, y_train, y_test = train_test_split(
-        features, targets, test_size=0.25
+        features, targets, test_size=0.25, random_state=42
     )
 
-    names = iris["target_names"]
-
-    # Create an XGBoost Classifier from the training data
+    # Create an XGBoost classifier with the best hyperparameters found by grid search
     xgboost_classifier = xgboost(x_train, y_train)
 
     # Display the confusion matrix of the classifier with both training and test sets
+    names = iris["target_names"]
     ConfusionMatrixDisplay.from_estimator(
         xgboost_classifier,
         x_test,
@@ -75,7 +70,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod(verbose=True)
     main()
